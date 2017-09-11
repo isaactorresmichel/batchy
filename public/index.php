41cb1f20b1bbc56eed355f0430a657fcc0cd8316
Isaac Torres <isaac@crayon.com.mx>
2017-09-11 13:50:14 -0500

    Add routes and services based on yml config files.

--- public/index.php
+++ public/index.php
@@ -1,30 +1,17 @@
 <?php
-if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
-    $url = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__ . $url['path'];
-    if (is_file($file)) {
-        return false;
-    }
+use Batchy\Utils\Server;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+if (Server::parseCliRequest()) {
+    return false;
 }
 
-require __DIR__ . '/../vendor/autoload.php';
-
 session_start();
-
-// Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require dirname(__DIR__) . '/src/settings.php';
 $app = new \Slim\App($settings);
 
-// Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
+Server::parseRoutes($app, dirname(__DIR__) . '/core.routes.yml');
+Server::parseServices($app, dirname(__DIR__) . '/core.services.yml');
 
-// Register middleware
-require __DIR__ . '/../src/middleware.php';
-
-// Register routes
-require __DIR__ . '/../src/routes.php';
-
-// Run app
 $app->run();
